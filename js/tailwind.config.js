e42e337e55ef3cec1c755e154002778ea0f1e8c5
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-10-17 22:43:36 -0300

    add: Configurações iniciais e Header finalizado

---
+++ js/tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#2571D1',
+                'secondary': '#F6F6F6',
+                'hover-btn': '#1B5DB1'
+            }
+        }
+    }
+}
